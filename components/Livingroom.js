4356c5072a4b674f06b5e25360464751d085e3e2
Finisher <fernandofine1@gmail.com>
2025-12-19 18:14:41 -0500

    upped padding

--- components/Livingroom.js
+++ components/Livingroom.js
@@ -8,7 +8,7 @@ const Livingroom = () => {
                 style={{ backgroundImage: 'url("/images/living.jpg")' }}
             >
                 <div className="p-4 text-pretty">
-                    <div className="text-3xl opacity-80 text-white lg:max-w-[50%] font-dince header-text text-pretty">
+                    <div className="text-3xl opacity-80 text-white lg:max-w-[50%] font-dince header-text text-pretty p-8">
                         Designed to expand with you.
                     </div>
                 </div>
